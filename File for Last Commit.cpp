c112df4548d7c16d564d7d42c3bb852c43adcb23
Prashant Singh <prashant.indigenous@gmail.com>
2022-10-05 18:07:15 +0530

    4th commit dummy

---
+++ File for Last Commit.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout << "enter the two values for addition\n";
+cin >> a>>b;
+c=a+b;
+cout << "the sum of "<<a<<" and "<<b<< " is "<<c;
+return 0;
+}
+
